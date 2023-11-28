6f2ac2cba46a1985f815575103fe2ff0e780b921
KPomales <kirstenpomales@gmail.com>
2023-11-28 15:26:57 +0100

    shareable picture

---
+++ jan_04_2021 copy.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+   include 'meta.php';
+    ?>
+    <style>
+    * {
+      box-sizing: border-box;
+    }
+
+    /* Create two equal columns that floats next to each other */
+    .column {
+      float: left;
+      width: 50%;
+      padding: 10px;
+      height: 300px; /* Should be removed. Only for demonstration */
+    }
+
+    /* Clear floats after the columns */
+    .row:after {
+      content: "";
+      display: table;
+      clear: both;
+    }
+    </style>
+  <!--OG Meta Tags-->
+  <meta property="og:image" content="http://kirstenpomales.com/img/protecting.png">
+  <meta property="og:url" content="https://kirstenpomales.com/sep_07_2020.html">
+  <meta property="og:site_name" content="Kirsten Pomales Langenbrunner"/>
+  <meta property="og:title" content="Protecting IP in Partnerships"/>
+  <meta property="og:description" content="A Scenario-Driven Analysis of IP Best Practices in B2B Partnerships"/>
+
+  <!--Twitter Meta Tags-->
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="@kirstenrpomales">
+  <meta name="twitter:creator" content="@kirstenrpomales">
+  <meta name="twitter:title" content="Protecting IP in Partnerships | Kirsten Pomales Langenbrunner">
+  <meta name="twitter:description" content="A Scenario-Driven Analysis of IP Best Practices in B2B Partnerships">
+  <meta name="twitter:image" content="http://kirstenpomales.com/img/protecting.png">
+  <!--END META TAGS-->  
+
+
+  <title>Blockchain: Enterprise Applications with the Fedex Institute | Kirsten Pomales Langenbrunner</title>
+
+</head>
+
+<body id="page-top">
+ <?php
+   include 'header.php';
+    ?>
+
+  <section class="features contact" id="about">
+    <div class="container">
+     <img style="width: 100%; height: auto; padding-bottom: 40px; padding-top: 60px;" src="img/fedex.png" alt="kirstenpomales">
+    </div>
+    <div class="container">
+      <div class="section-heading text-center">
+        <h3 class="text-left" style="font-weight: bold;">Blockchain: Enterprise Applications with the Fedex Institute</h3>
+        <h5 class = "text-left">A three-part event, hosted as part of the FedEx Institute of Technology Speaker Series
+        </h5>
+        <h6 class="text-left">Posted on August 1, 2020</h6>
+      
+        <br>
+        <p class="text-left">
+		"Blockchain: Enterprise Applications" was hosted as part of the <a class="text-info" href="https://www.memphis.edu/fedex/engagement/fit_blockchain901.php" target=_blank>FedEx Institute of Technology Speaker Series</a>, a virtual series on the current and future state of blockchain technology where tech leaders from Microsoft, IBM, IEEE, Consensys Health, and more shared their real world experience and provided insight into what lies ahead in the blockchain world.</p>
+		<br>
+		<p class="text-left">
+		During our three-part lecture, <a class="text-info" href="https://thomasbcox.com/" target=_blank>Thomas B. Cox</a> and I led a group of MBA students and executive leaders through the process of understanding and learning to apply blockchain technology to create solutions tailored to their company's needs.
+		<br>	</br>
+		<p class="text-left">
+		<a class="text-info" href="https://www.youtube.com/watch?v=A6iYFJiRjiU&ab_channel=FedExInstituteofTechnology" target=_blank>Lecture 1: Blockchain's Implications for Enterprises</a></p>
+		<h6 class="text-left">An exploration of the implications of blockchain technology for enterprises</h6>
+		<br>
+		<p class="text-left"><a class="text-info" href="https://www.youtube.com/watch?v=pDVNULPpPhA&ab_channel=FedExInstituteofTechnology" target=_blank>Lecture 2: Blockchain: Why Governance?</a></p>
+		<h6 class="text-left">A deep dive into why governance will play an integral role in the success of blockchain applications and networks</h6>
+		</br>
+		<p class="text-left"><a class="text-info" href="https://www.youtube.com/watch?v=VKUsp50POKg&ab_channel=FedExInstituteofTechnology" target=_blank>Lecture 3: Preparing Your Organization for Blockchain</a></p><h6 class="text-left">A workshop on preparing their organization to ideate on, evaluate, and adopt blockchain technology</h6>
+		</p>
+		<br>
+		<p class="text-left">
+		Thank you to the Fedex Institute and Blockchain901 for having us as guests! It was a pleasure to speak to your engaged group of students and professionals.</p>
+
+      </div>
+    </div>
+
+  </section>
+
+<?php
+   include 'footer.php';
+    ?>
+
+    <!-- JS -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="js/main.js"></script>
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+</html>
